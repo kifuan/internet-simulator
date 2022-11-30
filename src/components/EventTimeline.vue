82dd7ea28ebd10f5b87c701ac1cce6681d03f6c9
kifuan <kifuan@foxmail.com>
2022-11-30 13:22:44 +0800

    feat: timeline

--- src/components/EventTimeline.vue
+++ src/components/EventTimeline.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { useEventStore } from '../stores/event'
 
 const store = useEventStore()
+
+onMounted(async () => {
+  for (let i = 0; i < 10; i++) {
+    const action = store.chooseEvent().actions[0]
+    store.pushTimeline(action)
+    document.body.scrollIntoView({})
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+})
 </script>
 
 <template>
   <QTimeline color="secondary">
     <QTimelineEntry heading>
-      <h1 class="tw-text-xl">
-        Timeline heading
+      <h1 class="tw-text-4xl tw-font-semibold">
+        事件
       </h1>
     </QTimelineEntry>
 
-    <QTimelineEntry
-      subtitle="February 21, 1986"
-    >
-      Event content
-    </QTimelineEntry>
+    <TransitionGroup>
+      <QTimelineEntry
+        v-for="(timeline, index) in store.timeline"
+        :key="index"
+      >
+        <template #subtitle>
+          <div class="tw-text-xl">
+            {{ timeline.dateText }}
+          </div>
+        </template>
 
-    <QTimelineEntry
-      subtitle="February 21, 1986"
-    >
-      Event content
-    </QTimelineEntry>
+        <div class="tw-text-lg">
+          {{ timeline.eventText }}
+        </div>
+      </QTimelineEntry>
+    </TransitionGroup>
   </QTimeline>
 </template>
