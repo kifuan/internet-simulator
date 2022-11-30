448b0be364e39cbe1fe074dbc2753cc4eeec27a5
kifuan <kifuan@foxmail.com>
2022-11-30 14:30:17 +0800

    feat: timeline entry with detail

--- src/components/TimelineEntry.vue
+++ src/components/TimelineEntry.vue
@@ -20,8 +20,26 @@ onMounted(() => {
       </div>
     </template>
 
-    <div class="tw-text-lg">
-      {{ item.eventText }}
+    <div class="tw-text-lg tw-flex tw-flex-col tw-space-y-2">
+      <div>
+        {{ item.eventText }}
+      </div>
+
+      <div>
+        > {{ item.actionText }}
+      </div>
+
+      <div>
+        {{ item.actionMessage }}
+      </div>
+
+      <div
+        v-for="(message, index) in item.actionEffectMessages"
+        :key="index"
+        class="tw-text-zinc-400"
+      >
+        {{ message }}
+      </div>
     </div>
   </QTimelineEntry>
 </template>
